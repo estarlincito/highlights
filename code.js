f6c6b63beb9a41fea3f55907627f4598960a21fa
Estarlin R <estarlincito@EstarlincitoM.local>
2022-08-27 17:31:11 -0400

    0.012

--- code.js
+++ code.js
@@ -1,27 +1,26 @@
-const highlights = document.getElementById('highlights');
+const highlights = document.getElementById("highlights");
 
 //data__highlights
 fetch("data.json")
-.then(response => response.json())
-.then(data => {
-
+  .then((response) => response.json())
+  .then((data) => {
     //set__localStorage
-    if(!localStorage.status){
-        localStorage.setItem('status', 1);
-    }else{
-        localStorage.status++;
+    if (!localStorage.status) {
+      localStorage.setItem("status", 1);
+    } else {
+      localStorage.status++;
     }
 
     //reset__localStorage
-    if(localStorage.status >= data.length){
-        localStorage.status = 1;
+    if (localStorage.status >= data.length) {
+      localStorage.status = 1;
     }
-        
-    const status = localStorage.getItem('status');
+
+    const status = localStorage.getItem("status");
 
     highlights.innerHTML = `
     <div class="container__text">
         <a href="${data[status].url}" target="_blank">${data[status].text}</a>
     </div>
     `;
-});
+  });
